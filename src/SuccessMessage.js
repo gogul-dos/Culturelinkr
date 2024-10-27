2b2480c772651d0c9aad1af1de53a95039517bda
gogulakrishnan R <gogulakrishnan8870@gmail.com>
2024-10-27 16:45:55 +0530

    Final

---
+++ src/SuccessMessage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SuccessMessage = () => (
+  <div>
+    <h3>Form submitted successfully!</h3>
+    <p>Thanks for reaching out. We’ll respond soon.</p>
+  </div>
+);
+
+export default SuccessMessage;
